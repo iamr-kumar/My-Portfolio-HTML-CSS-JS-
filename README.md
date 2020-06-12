afab7e33077b62a2444cb5b533f6769245339afb
Ritik Kumar <ritik.kumar006@gmail.com>
2020-06-13 03:51:45 +0530

    Add blog and portfolio section

---
+++ README.md
@@ -0,0 +1,1 @@
+My Portfolio web page using HTML, CSS and JavaScript
---
+++ index.js
@@ -0,0 +1,79 @@
+
+
+function toggleNav(){
+  var nav = document.getElementById("mySidenav");
+  var main = document.getElementById("main");
+  var icon = document.getElementById("nav-open-close");
+  var button = document.getElementById("nav-button");
+  if(nav.style.width === "0px" && main.style.marginLeft === "0px"){
+    nav.style.width = "340px";
+    nav.style.borderRight = "2px solid #c9ab81";
+    main.style.marginLeft = "340px";
+  }
+  else{
+    nav.style.width = "0px";
+    nav.style.borderRight = "none";
+    main.style.marginLeft = "0px";
+  }
+  icon.classList.toggle("fa-arrow-right");
+  icon.classList.toggle("fa-arrow-left");
+  icon.classList.toggle("op");
+}
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
